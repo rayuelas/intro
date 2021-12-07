3f44474740be07a20f6aa1f12dd3caa15c694f95
beatriz <95693996+rayuelas@users.noreply.github.com>
2021-12-07 10:58:28 -0300

    Add files via upload

---
+++ lista3.py
@@ -0,0 +1,33 @@
+#Lista 3: Variáveis, entrada e saida
+#Exercício 1
+num1 = float (input ('Escreva um numero: '))
+num2 = float (input ('Escreva outro numero: '))
+print(num1>num2)
+
+#Exercício 2
+num1 = float (input ('Escreva um numero: ') )
+num2 = float (input ('Escreva outro numero: ') )
+print(num1>0 and num2>0)
+
+#Exercício 3
+num = float (input ('Escreva um numero: '))
+print(num > 0 and num < 1)
+
+#Exercício 4
+num1 = float( input('escreva um numero: ') )
+num2 = float ( input ('escreva outro numero: ') )
+num3 = float ( input ('escreva outro numero: ') )
+print(num1 != num2 and num1 != num3 and num2 != num3)
+
+#Exercício 5
+num1 = float( input('escreva um numero: ') )
+num2 = float ( input ('escreva outro numero: ') )
+num3 = float ( input ('escreva outro numero: ') )
+print( num1 < num2 < num3)
+
+#Exercício 6
+num = float ( input ('escreva um numero: ') )
+print(num%2 == 0)
+
+
+            
